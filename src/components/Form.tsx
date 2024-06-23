d0e4f6d25535057b135084d93a5a1e9fa4c1defd
rodri <ojox_nike@hotmail.com>
2024-06-23 12:40:28 -0600

    Se añadio el README.md

---
+++ src/components/Form.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import {categories} from '../data/categories'
+
+export default function Form() {
+	return (
+		<form
+			className="space-y-5 bg-white shadow p-10 rounded-lg"
+		>
+			<div className="grid grid-cols-1 gap-3">
+				<label htmlFor="category" className="font-bold">Categoría:</label>
+				<select 
+					className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+					name="" 
+					id="category"
+				>
+					{categories.map(category => (
+						<option 
+							key={category.id}
+							value={category.id}
+						>
+							{category.name}	
+						</option>
+					))}
+				</select>
+			</div>
+
+			<div className="grid grid-cols-1 gap-3">
+				<label htmlFor="activity" className="font-bold">Actividad:</label>
+				<input 
+					id="activity"
+					type="text"
+					className='border-slate-300 p-2 rounded-lg'
+					placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+				/>
+			</div>
+
+			<div className="grid grid-cols-1 gap-3">
+				<label htmlFor="calories" className="font-bold">Calorias:</label>
+				<input 
+					id="calories"
+					type="number"
+					className='border-slate-300 p-2 rounded-lg'
+					placeholder="Calorias. ej. 300 o 500"
+				/>
+			</div>
+
+			<input 
+				type='submit'
+				className='bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer'
+				value="Guardar Comida o Guardar Ejercicio"
+			/>
+		</form>
+	)
+}
