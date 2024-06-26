525ff601aa4b5053f542f13bc2db93fb2a5704c7
rodri <ojox_nike@hotmail.com>
2024-06-25 18:12:56 -0600

    Agregando useState para el formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import {useState} from 'react'
 
 import {categories} from '../data/categories'
 
 export default function Form() {
+	const [activity, setActivity] = useState({
+		category: 1,
+		name: '',
+		calories: 0
+	})
+
 	return (
 		<form
 			className="space-y-5 bg-white shadow p-10 rounded-lg"
@@ -11,7 +17,8 @@ export default function Form() {
 				<label htmlFor="category" className="font-bold">Categoría:</label>
 				<select 
 					className="border border-slate-300 p-2 rounded-lg w-full bg-white"
-					name="" 
+					name=""
+					type="number"
 					id="category"
 				>
 					{categories.map(category => (
@@ -26,9 +33,9 @@ export default function Form() {
 			</div>
 
 			<div className="grid grid-cols-1 gap-3">
-				<label htmlFor="activity" className="font-bold">Actividad:</label>
+				<label htmlFor="name" className="font-bold">Actividad:</label>
 				<input 
-					id="activity"
+					id="name"
 					type="text"
 					className='border-slate-300 p-2 rounded-lg'
 					placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
